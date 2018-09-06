a2f71e385d4d0535bd4f3521ab26fb964526ff98
Devraj Singh <devrajsingh06@gmail.com>
2018-09-07 02:54:46 +0800

    Test Project

---
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestNgMavenExample {
+
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+}
---
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestNgMavenSecondClass {
+
+	@Test
+	public void oneMoreTest() {
+		System.out.println("This is a TestNG-Maven based test");
+	}
+}
